d8ed87eee7084091f8aa70e599c1f9b56c85864c
David López <davidlopezsaludes@enti.com>
2018-05-01 21:43:06 +0200

    Checkpoints & respawns

---
+++ Disseny-de-nivells/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public LvlManager lvlManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        lvlManager = FindObjectOfType<LvlManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.name == "Player")
+        {
+            lvlManager.currentCheckPoint = gameObject;
+        }
+    }
+}
---
+++ Disseny-de-nivells/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    public LvlManager lvlManager;
+
+	// Use this for initialization
+	void Start () {
+        lvlManager = FindObjectOfType<LvlManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other){
+        
+        if(other.name == "Player")
+        {
+            lvlManager.RespawnPlayer();
+        }
+    }
+}
